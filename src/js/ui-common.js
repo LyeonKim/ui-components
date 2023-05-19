8b497c6bddf138f3991b14b934d0456f1adb18c0
Lyeonkim <87533823+LyeonKim@users.noreply.github.com>
2023-05-19 18:09:38 +0900

    file edit : ui-common.js && html, css 수정사항

--- src/js/ui-common.js
+++ src/js/ui-common.js
@@ -11,6 +11,36 @@ const UI = ((window, document, $) => {
     init: () => {
       const html = document.documentElement;
 
+      // drop-menu
+      document.addEventListener('click', (event) => {
+        if(event.target.closest('.drop-menu')){ //closest(선택자) = 해당 선택자와 동일한 요소를 찾을 때 까지 -> e.target은 자신 ~ 부모를 순회하며 찾는다.
+          const dropBtn  = event.target;
+          const dropMenu = dropBtn.closest('.drop-menu');
+          const isOpen = dropMenu.classList.contains('is-open');
+
+          const closeDropMenu = () => { dropMenu.classList.remove('is-open') };
+
+          const activeDropMenu = () => { 
+            dropMenu.classList.add('is-open');
+
+            document.addEventListener('click', (item) => {
+              if(item.target.type == 'button' && !item.target.classList.contains('drop-btn')) {
+                dropBtn.innerText = item.target.innerText;
+                closeDropMenu();
+              }
+            });
+          }
+
+          isOpen ? closeDropMenu() : activeDropMenu();
+
+          document.addEventListener('click', (e) => {
+            if(!e.target.closest('.drop-menu')) {
+              closeDropMenu();
+            }
+          });
+
+        }
+      });
       html.classList.add('init');
     }
   };
